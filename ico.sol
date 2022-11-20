f5918d8c3d77ef15ef15296529a326bd9f97e1e3
dmitrykroo <47831268+dmitrykroo@users.noreply.github.com>
2022-11-20 17:36:02 +0700

    HW2

---
+++ ico.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ICO is ERC20{
+    address payable private _creator;
+
+    uint private immutable _hardCap;
+    uint8 private immutable _decimals;
+    uint private immutable _multiplier;
+    uint private immutable _priceInWei;
+
+    bool _mintActive;
+
+    constructor(uint hardCap_, uint8 decimals_, uint priceInWei_, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        _hardCap = hardCap_;
+        _decimals = decimals_;
+        _multiplier = 10 ** decimals_;
+        _priceInWei = priceInWei_;
+        _creator = payable(msg.sender);
+        _mintActive = true;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function _cappedMint(address account, uint256 amount) internal {
+        require(totalSupply() + amount <= _hardCap, "Hard cap reached");
+        _mint(account, amount);
+    }
+
+    function buy() public payable {
+        require(_mintActive, "ICO closed");
+        uint token_amount = msg.value / _priceInWei * _multiplier;
+        _cappedMint(msg.sender, token_amount);
+        _creator.transfer(msg.value);
+    }
+
+    function stopMint() public {
+        require(msg.sender == _creator);
+        _mintActive = false;
+    }
+}
